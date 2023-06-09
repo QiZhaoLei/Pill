7c0c77c2d06d7a46a466fec358c14f94beeb1a8e
qzl <3468250242@qq.com>
2023-06-09 23:05:44 +0800

    后端开发-自动代码生成模式开发

--- pill-admin/src/main/java/com/qzl/web/controller/pill/PillFactoryController.java
+++ pill-admin/src/main/java/com/qzl/web/controller/pill/PillFactoryController.java
@@ -1,35 +1,98 @@
 package com.qzl.web.controller.pill;
 
+import com.qzl.common.annotation.Log;
 import com.qzl.common.core.controller.BaseController;
 import com.qzl.common.core.domain.AjaxResult;
 import com.qzl.common.core.page.TableDataInfo;
+import com.qzl.common.enums.BusinessType;
+import com.qzl.common.utils.poi.ExcelUtil;
 import com.qzl.pill.domain.PillFactory;
 import com.qzl.pill.service.IPillFactoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
+ * 生产厂家信息Controller
+ * 
  * @author 齐兆磊
- * @create 2023/06/09
+ * @date 2023-06-09
  */
 @RestController
 @RequestMapping("/pill/factory")
-public class PillFactoryController extends BaseController {
-
+public class PillFactoryController extends BaseController
+{
     @Autowired
     private IPillFactoryService pillFactoryService;
 
+    /**
+     * 查询生产厂家信息列表
+     */
     @PreAuthorize("@ss.hasPermi('pill:factory:list')")
     @GetMapping("/list")
-    public TableDataInfo list(PillFactory factory) {
+    public TableDataInfo list(PillFactory pillFactory)
+    {
         startPage();
-        List<PillFactory> list = pillFactoryService.selectPillFactoryList(factory);
+        List<PillFactory> list = pillFactoryService.selectPillFactoryList(pillFactory);
         return getDataTable(list);
     }
 
+    /**
+     * 导出生产厂家信息列表
+     */
+    @PreAuthorize("@ss.hasPermi('pill:factory:export')")
+    @Log(title = "生产厂家信息", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, PillFactory pillFactory)
+    {
+        List<PillFactory> list = pillFactoryService.selectPillFactoryList(pillFactory);
+        ExcelUtil<PillFactory> util = new ExcelUtil<PillFactory>(PillFactory.class);
+        util.exportExcel(response, list, "生产厂家信息数据");
+    }
+
+    /**
+     * 获取生产厂家信息详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('pill:factory:query')")
+    @GetMapping(value = "/{factoryId}")
+    public AjaxResult getInfo(@PathVariable("factoryId") Long factoryId)
+    {
+        return success(pillFactoryService.selectPillFactoryByFactoryId(factoryId));
+    }
+
+    /**
+     * 新增生产厂家信息
+     */
+    @PreAuthorize("@ss.hasPermi('pill:factory:add')")
+    @Log(title = "生产厂家信息", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody PillFactory pillFactory)
+    {
+        return toAjax(pillFactoryService.insertPillFactory(pillFactory));
+    }
+
+    /**
+     * 修改生产厂家信息
+     */
+    @PreAuthorize("@ss.hasPermi('pill:factory:edit')")
+    @Log(title = "生产厂家信息", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody PillFactory pillFactory)
+    {
+        return toAjax(pillFactoryService.updatePillFactory(pillFactory));
+    }
+
+    /**
+     * 删除生产厂家信息
+     */
+    @PreAuthorize("@ss.hasPermi('pill:factory:remove')")
+    @Log(title = "生产厂家信息", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{factoryIds}")
+    public AjaxResult remove(@PathVariable Long[] factoryIds)
+    {
+        return toAjax(pillFactoryService.deletePillFactoryByFactoryIds(factoryIds));
+    }
 }
--- pill-pill/src/main/java/com/qzl/pill/domain/PillFactory.java
+++ pill-pill/src/main/java/com/qzl/pill/domain/PillFactory.java
@@ -1,108 +1,126 @@
 package com.qzl.pill.domain;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.qzl.common.annotation.Excel;
 import com.qzl.common.core.domain.BaseEntity;
 
 /**
+ * 生产厂家信息对象 pill_factory
+ * 
  * @author 齐兆磊
- * @create 2023/06/09
+ * @date 2023-06-09
  */
-public class PillFactory extends BaseEntity {
+public class PillFactory extends BaseEntity
+{
     private static final long serialVersionUID = 1L;
-    /**
-     * 厂家ID
-     */
+
+    /** 厂家ID */
     private Long factoryId;
-    /**
-     * 厂家名称
-     */
+
+    /** 厂家名称 */
+    @Excel(name = "厂家名称")
     private String factoryName;
-    /**
-     * 厂家编码
-     */
+
+    /** 厂家编码 */
+    @Excel(name = "厂家编码")
     private String factoryCode;
-    /**
-     * 联系人
-     */
+
+    /** 联系人 */
+    @Excel(name = "联系人")
     private String contact;
-    /**
-     * 电话
-     */
+
+    /** 电话 */
+    @Excel(name = "电话")
     private String phone;
-    /**
-     * 关键字
-     */
+
+    /** 关键字 */
+    @Excel(name = "关键字")
     private String keyword;
-    /**
-     * 状态
-     */
+
+    /** 状态 */
+    @Excel(name = "状态")
     private String status;
 
-    public Long getFactoryId() {
-        return factoryId;
-    }
-
-    public void setFactoryId(Long factoryId) {
+    public void setFactoryId(Long factoryId) 
+    {
         this.factoryId = factoryId;
     }
 
-    public String getFactoryName() {
-        return factoryName;
+    public Long getFactoryId() 
+    {
+        return factoryId;
     }
-
-    public void setFactoryName(String factoryName) {
+    public void setFactoryName(String factoryName) 
+    {
         this.factoryName = factoryName;
     }
 
-    public String getFactoryCode() {
-        return factoryCode;
+    public String getFactoryName() 
+    {
+        return factoryName;
     }
-
-    public void setFactoryCode(String factoryCode) {
+    public void setFactoryCode(String factoryCode) 
+    {
         this.factoryCode = factoryCode;
     }
 
-    public String getContact() {
-        return contact;
+    public String getFactoryCode() 
+    {
+        return factoryCode;
     }
-
-    public void setContact(String contact) {
+    public void setContact(String contact) 
+    {
         this.contact = contact;
     }
 
-    public String getPhone() {
-        return phone;
+    public String getContact() 
+    {
+        return contact;
     }
-
-    public void setPhone(String phone) {
+    public void setPhone(String phone) 
+    {
         this.phone = phone;
     }
 
-    public String getKeyword() {
-        return keyword;
+    public String getPhone() 
+    {
+        return phone;
     }
-
-    public void setKeyword(String keyword) {
+    public void setKeyword(String keyword) 
+    {
         this.keyword = keyword;
     }
 
-    public String getStatus() {
-        return status;
+    public String getKeyword() 
+    {
+        return keyword;
+    }
+    public void setStatus(String status) 
+    {
+        this.status = status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public String getStatus() 
+    {
+        return status;
     }
 
     @Override
     public String toString() {
-        return "PillFactory{" +
-                "factoryId=" + factoryId +
-                ", factoryName='" + factoryName + '\'' +
-                ", factoryCode='" + factoryCode + '\'' +
-                ", contact='" + contact + '\'' +
-                ", phone='" + phone + '\'' +
-                ", keyword='" + keyword + '\'' +
-                ", status='" + status + '\'' +
-                '}';
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("factoryId", getFactoryId())
+            .append("factoryName", getFactoryName())
+            .append("factoryCode", getFactoryCode())
+            .append("contact", getContact())
+            .append("phone", getPhone())
+            .append("keyword", getKeyword())
+            .append("status", getStatus())
+            .append("createBy", getCreateBy())
+            .append("createTime", getCreateTime())
+            .append("updateBy", getUpdateBy())
+            .append("updateTime", getUpdateTime())
+            .append("remark", getRemark())
+            .toString();
     }
 }

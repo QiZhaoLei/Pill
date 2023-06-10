cb478758c875d43f973ef5e0d5510fea1a8ea307
qzl <3468250242@qq.com>
2023-06-10 16:45:39 +0800

    药品模块-查询调整-多表连接

--- pill-admin/src/test/java/com/qzl/PssTest.java
+++ pill-admin/src/test/java/com/qzl/PssTest.java
@@ -1,10 +1,14 @@
 package com.qzl;
 
+import com.qzl.pill.domain.PillDrug;
 import com.qzl.pill.domain.PillFactory;
+import com.qzl.pill.service.IPillDrugService;
 import com.qzl.pill.service.IPillFactoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 /**
  * @author 齐兆磊
@@ -16,10 +20,27 @@ public class PssTest {
     @Autowired
     private IPillFactoryService pillFactoryService;
 
+    @Autowired
+    private IPillDrugService pillDrugService;
+
     @Test
     public void testSelectFactory() {
         PillFactory pillFactory = new PillFactory();
         pillFactory.setFactoryName("云南");
         System.out.println(pillFactoryService.selectPillFactoryList(pillFactory));
     }
+
+    @Test
+    public void testSelectDrug() {
+        PillDrug pillDrug = new PillDrug();
+        PillFactory pillFactory = new PillFactory();
+        pillFactory.setFactoryName("哈药");
+        pillDrug.setPillFactory(pillFactory);
+        List<PillDrug> pillDrugs = pillDrugService.selectPillDrugList(pillDrug);
+        System.out.println(pillDrugs);
+    }
+
+
+
+
 }
